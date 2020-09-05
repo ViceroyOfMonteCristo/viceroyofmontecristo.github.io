ec5c1db9a3faaf01b28b1472f7b7a19b2d56dbf0
ViceroyOfMonteCristo <68447856+ViceroyOfMonteCristo@users.noreply.github.com>
2020-09-05 19:25:19 -0400

    Second Half Split

---
+++ hosting/Sburg12/build.sh
@@ -0,0 +1,2 @@
+cat libs/Iuppiter.js libs/modernizr.js src/Sburb.js src/Sprite.js src/Fighter.js src/Character.js src/SpriteButton.js src/Animation.js src/Room.js src/FontEngine.js src/Action.js src/events.js src/Trigger.js src/commands.js src/serialization.js src/Dialoger.js src/Chooser.js src/Audio.js src/Assets.js src/Debugger.js src/Path.js src/ActionQueue.js > Sburb.js
+node_modules/jsmin/bin/jsmin < Sburb.js > Sburb.min.js
