fd0e17e8dee482a68459705bcf567636b4bdb900
ViceroyOfMonteCristo <68447856+ViceroyOfMonteCristo@users.noreply.github.com>
2020-09-05 19:05:43 -0400

    Sburg 2 Split

---
+++ hosting/Sburg2/projects/altgenbound/config.js
@@ -0,0 +1,68 @@
+// Configs!
+
+var SburbConfig = {
+  "characterSpeed": 8,
+
+  "chooserFill1": "#7289DA",
+  "chooserFill2": "#7289DA",
+  "chooserFill3": "#23272A",
+  "chooserCursor": "#ffffff",
+  "chooserCursorSelected": "#cccccc",
+
+  "prefixColours": {
+    "dickle":"#3478ED",
+    "node":"#FF00FF",
+    "guy":"#1C1CED",
+    "cheeto":"#208E99",
+    "swifter":"#FF2364",
+    "holly":"#E3ABEA",
+    "liz":"#e83bff",
+    "vice":"#cc0a0a",
+    "vrachne":"#1025C3",
+    "bowman":"#720720",
+    "makin":"#AA72C0"
+  },
+
+  "characterAnimation": {
+    "idleFront": {
+      "startPos": 0,
+      "length": 1,
+      "frameInterval": 2
+    },
+    "idleRight": {
+      "startPos": 1,
+      "length": 1,
+      "frameInterval": 2
+    },
+    "idleBack": {
+      "startPos": 2,
+      "length": 1,
+      "frameInterval": 2
+    },
+    "idleLeft": {
+      "startPos": 3,
+      "length": 1,
+      "frameInterval": 2
+    },
+    "walkFront": {
+      "startPos": 8,
+      "length": 4,
+      "frameInterval": 4
+    },
+    "walkRight": {
+      "startPos": 12,
+      "length": 4,
+      "frameInterval": 4
+    },
+    "walkBack": {
+      "startPos": 16,
+      "length": 4,
+      "frameInterval": 4
+    },
+    "walkLeft": {
+      "startPos": 20,
+      "length": 4,
+      "frameInterval": 4
+    }
+  }
+}
